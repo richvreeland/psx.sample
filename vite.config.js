b7bc664e235af6a26f9d057a58c67d72719ea156
Rich Vreeland <1592343+richvreeland@users.noreply.github.com>
2026-01-20 10:14:13 -0800

    prep for single HTML style

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/REPO-NAME-HERE/', // Replace with your repo name
-})
